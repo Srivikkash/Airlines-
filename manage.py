c0eff8f79910ecd231d3291f3a5916d645e95e0e
DINEESH R <64298980+dineeshr@users.noreply.github.com>
2023-12-14 20:50:29 +0530

    Add files via upload

--- manage.py
+++ manage.py
@@ -4,16 +4,15 @@ from tkinter import messagebox
 from tkinter import ttk
 import db
 
-connection = db.create_connection()
-
+connection=db.create_connection()
 
 def cancel():
+    global enter6
     global root_cancel
     root_cancel = Tk()
     root_cancel.config(bg="beige")
     root_cancel.state("zoomed")
-    root_cancel.title("Manage")
-
+    root_cancel.title("CANCEL")
     # manage
 
     cursor = connection.cursor()
@@ -33,36 +32,37 @@ def cancel():
         tree.column(col, width=100, anchor=tk.CENTER)  # Adjust width as needed
 
     # Populate the Treeview with data
-
     # Add custom colors to alternating rows
     for i, ro in enumerate(results):
         if i % 2 == 0:
-            tree.tag_configure('evenrow', background='#f0f0f0')
-            tree.insert('', 'end', values=ro, tags=('evenrow',))
+            tree.tag_configure('evenrow', background='#ffb6c1')
+            tree.insert('', 'end',values=ro, tags=('evenrow',))
         else:
-            tree.insert('', 'end', values=ro)
+            tree.tag_configure('oddrow', background='#ADD8E6')
+            tree.insert('', 'end',values=ro, tags=('oddrow',))
     # Pack the Treeview widget
 
     # Pack the Treeview widget
     tree.pack(expand=True, fill="both")
     tree.pack()
-
+    Label(root_cancel, font=("arial", 15, 'bold'),
+          text="Cancel Booking").place(x=600, y=500)
     Label(root_cancel, font=("arial", 15, 'bold'),
           text="Ticket Number : ").place(x=350, y=550)
     enter6 = Entry(root_cancel, width=35)
     enter6.place(x=600, y=550)
-
     def de():
-        tno = enter6.get()
-        Delete = "delete from booking where Ticket_NO="+tno
+        tno=enter6.get()
+        Delete="delete from booking where Ticket_NO="+tno
         cursor.execute(Delete)
-        messagebox.showinfo("Information", "TICKET CANCELLED")
+        connection.commit()
+        messagebox.showinfo("Information","TICKET CANCELLED")
         root_cancel.destroy()
         messagebox.showinfo("Success", "You have cancelled your flight")
-        messagebox.showinfo(
-            "Refund", "60%% refund has been credited to your account")
+        messagebox.showinfo("Refund", "60%% refund has been credited to your account")
     btn_c = Button(root_cancel, text="Cancel Flight", font=(
         "cursive", 22, 'bold'), activebackground="tan", command=de)
     btn_c.place(x=600, y=600)
-
+    
     root_cancel.mainloop()
+   
